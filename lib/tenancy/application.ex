f65988acd8427500a44b05c1ad46b3477a263c45
Stephen Bussey <sb8244@cs.ship.edu>
2019-10-24 00:55:00 -0400

    Remove TenancyWeb

--- lib/tenancy/application.ex
+++ lib/tenancy/application.ex
@@ -7,20 +7,12 @@ defmodule Tenancy.Application do
 
   def start(_type, _args) do
     children = [
-      Tenancy.Repo,
-      TenancyWeb.Endpoint
+      Tenancy.Repo
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Tenancy.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
-  def config_change(changed, _new, removed) do
-    TenancyWeb.Endpoint.config_change(changed, removed)
-    :ok
   end
 end
